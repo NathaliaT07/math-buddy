1efd994a503cfb4e1a97829ea52a35e0e66c3db4
NathaliaT07 <Nathalia.Torres975@icloud.com>
2024-07-10 12:11:26 -0500

    added timer and score keeper

--- app/add.js
+++ app/add.js
@@ -3,7 +3,13 @@ const option2 = document.getElementById("option2");
 const option3 = document.getElementById("option3");
 const audio = document.getElementById("wrong");
 
+const currentScore = document.getElementById("current-score");
+const timerDisplay = document.getElementById("timer");
+
 let answer = 0;
+let score = 0;
+let timeLeft = 100;
+let timer;
 
 function generateEquation(){
     let num1 = Math.floor(Math.random() * 13);
@@ -41,9 +47,52 @@ function generateEquation(){
     option3.innerHTML = switchAnswers[2];
 };
 
+function updateScore(){
+    currentScore.textContent = score;
+}
+
+function updateTimer() {
+    timerDisplay.textContent = timeLeft;
+}
+
+function startGame() {
+    score = 0;
+    timeLeft = 60;
+    generateEquation();
+    updateScore();
+    updateTimer();
+
+    clearInterval(timer);
+    timer = setInterval(function (){
+        timeLeft--;
+        updateTimer();
+        if(timeLeft === 0){
+            clearInterval(timer);
+
+            window.scrollTo(0,0);
+
+            let finalScore = score;
+            alert("Time is Up! YOur Score is "+ finalScore);
+
+            //reloading the page
+            location.reload();
+        }
+    }, 1000)
+}
+
+function updateTime() {
+    timeLeft--;
+    document.getElementById("timer").innerHTML = timeLeft;
+    if(timeLeft === 0){
+        alert("Your time is up! Your final score is " + score + ".");
+    }
+}
+
 option1.addEventListener("click", function(){
     if (option1.innerHTML == answer){
+        score++;
         generateEquation();
+        updateScore();
     } else {
         audio.play();
     }
@@ -51,7 +100,9 @@ option1.addEventListener("click", function(){
 
 option2.addEventListener("click", function(){
     if (option2.innerHTML == answer){
+        score++;
         generateEquation();
+        updateScore();
     } else {
         audio.play();
     }
@@ -59,7 +110,9 @@ option2.addEventListener("click", function(){
 
 option3.addEventListener("click", function(){
     if (option3.innerHTML == answer){
+        score++;
         generateEquation();
+        updateScore();
     } else {
         audio.play();
     }
@@ -67,3 +120,5 @@ option3.addEventListener("click", function(){
 
 
 generateEquation();
+startGame();
+updateScore();
